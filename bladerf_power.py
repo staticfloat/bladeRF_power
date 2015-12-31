8eee6aee5d7bce23c6d69e30e37629931f2d5f53
Elliot Saba <staticfloat@gmail.com>
2015-12-30 17:02:03 -0800

    Look only at a subsection of the FFT output to combat DC offsets and leaky antialiasing filters

--- bladerf_power.py
+++ bladerf_power.py
@@ -12,25 +12,25 @@ Arguments:
 Options:
   -h --help                Show this screen.
   -v --version             Show version.
-  -d --device=<d>          Device identifier [default: ]
   -f --file=<f>            File to write to [default: output.csv].
+  -z --compress            Compress output with gzip on-the-fly [default: False]
+  -e --exit-timer=<et>     Set capture time (example: 5h23m2s) [default: 0]
   -b --bandwidth=<bw>      Capture bandwidth [default: 28M].
-  -n --num-buffers=<nb>    Number of transfer buffers [default: 16].
-  -t --num-transfers=<nt>  Number of transfers [default: 16].
-  -l --num-samples=<ns>    Numper of samples per transfer buffer [default: 8192].
+  -M --filter-margin=<fm>  Anti-aliasing filter margin [default: .85]. This value
+                           is combined with bandwidth to view only a portion of
+                           the captured signal to combat leaky anti-aliasing
+                           filters. Actual useful signal bandwidth is fm*bw/2.
+  -W --window-type=<wt>    Set FFT analysis windowing function [default: hann]
   -g --lna-gain=<g>        Set LNA gain [default: LNA_GAIN_MAX]
   -o --rx-vga1=<g>         Set vga1 gain [default: RXVGA1_GAIN_MIN]
   -w --rx-vga2=<g>         Set vga2 gain [default: RXVGA2_GAIN_MIN]
+  -d --device=<d>          Device identifier [default: ]
+  -n --num-buffers=<nb>    Number of transfer buffers [default: 16].
+  -t --num-transfers=<nt>  Number of transfers [default: 16].
+  -l --num-samples=<ns>    Numper of samples per transfer buffer [default: 8192].
   -P --num-workers=<p>     Set number of FFT workers [default: 2]
-  -e --exit-timer=<et>     Set capture time (example: 5h23m2s) [default: 0]
-  -W --window-type=<wt>    Set window type [default: hann]
-  --demean                 Demean signal before taking FFT
-  -z                       Compress output with gzip on-the-fly [default: False]
 """
 import sys
-import bladeRF
-import IPython
-import ipdb
 from numpy import *
 from docopt import docopt
 
@@ -82,6 +82,7 @@ def intish(x):
     return int(floatish(x))
 
 def int_or_attr(x):
+    import bladeRF
     try:
         return int(x)
     except:
@@ -116,10 +117,16 @@ import Queue
 def file_worker(q_file, outfile, compress):
     import subprocess
     import os
-    if compress:
-        f = subprocess.Popen("gzip -9 -c > %s"%(outfile), shell=True, stdin=subprocess.PIPE, preexec_fn=os.setpgrp).stdin
+    if outfile == '-':
+        if compress:
+            f = subprocess.Popen("gzip -9 -c", shell=True, stdin=subprocess.PIPE, preexec_fn=os.setpgrp).stdin
+        else:
+            f = sys.stdout
     else:
-        f = open(outfile, 'w')
+        if compress:
+            f = subprocess.Popen("gzip -9 -c > %s"%(outfile), shell=True, stdin=subprocess.PIPE, preexec_fn=os.setpgrp).stdin
+        else:
+            f = open(outfile, 'w')
 
     try:
         while True:
@@ -133,7 +140,8 @@ def file_worker(q_file, outfile, compress):
     except KeyboardInterrupt:
         pass
 
-    f.close()
+    if not (outfile == "-" and compress):
+        f.close()
     print "Gracefully exited file_worker"
 
 def start_worker_pool(num_workers, outfile, compress):
@@ -170,32 +178,71 @@ def db(x):
     from numpy import log10, abs
     return 20*log10(abs(x))
 
-def analyze_view(data, window_func, demean, center_freq, bandwidth, end_freq, timestamp, q_file):
-    from numpy.fft import fft, fftshift
+def analyze_view(data, window_func, lower_sideband, center_freq, analysis_bandwidth, bin_width, start_freq, end_freq, timestamp, q_file):
+    """
+    Perform power spectral analysis on data of length fft_len, passing it off to
+    be written to file afterward.
+
+    Parameters
+    ----------
+    data : array (real/complex interleaved)
+        Incoming SC16 interleaved data samples of length fft_len
+
+    window_func : function
+        FFT windowing function, such as scipy.signal.hann()
+
+    lower_sideband : bool
+        Whether the lower or upper sideband of this view should be analyzed
+
+    center_freq : float (Hz)
+        Center frequency of this view
+
+    analysis_bandwidth : float (Hz)
+        The effective bandwidth we analyze in each view
+
+    bin_width : float (Hz)
+        The width of each FFT bin, in Hertz
+
+    start_freq : float (Hz)
+        The lower edge of the region of interest in frequency
+
+    end_freq : float (Hz)
+        The upper edge of the region of interest in frequency
+
+    timestamp : float (seconds)
+        The timestamp for this buffer
+
+    q_file : Queue
+        Queue used to communicate with file thread
+    """
+    from numpy.fft import fft
 
     # Convert data from sc16 into complex data
     data = data[::2] + 1j*data[1::2]
 
-    if demean:
-        data = data - mean(data)
-
     # Take FFT of this data
-    DATA = fftshift(fft(data * window_func(len(data))))
+    DATA = fft(data * window_func(len(data)))
 
-    # Find start/end frequencies that we get from this FFT
-    start_freq = center_freq - bandwidth/2
-    end_freq = min(start_freq + bandwidth, end_freq)
+    # Find start/end frequencies that we get from this FFT, and which bins we
+    # want to slice out of the DATA array
+    if lower_sideband:
+        view_start = max(center_freq - analysis_bandwidth, start_freq)
+        view_end = min(center_freq - bin_width, end_freq)
 
-    # Figure out if we actually want all of the bins in our FFT
-    num_bins = len(DATA)
-    if start_freq + bandwidth > end_freq:
-        num_bins = int(ceil(len(DATA)*((end_freq - start_freq)/bandwidth)))
+        bin_start = int(round((view_start - center_freq)/bin_width)) + len(DATA)
+        bin_end = int(round((view_end - center_freq)/bin_width)) + len(DATA) + 1
+    else:
+        view_start = max(center_freq + bin_width, start_freq)
+        view_end = min(center_freq + analysis_bandwidth, end_freq)
+
+        bin_start = int(round((view_start - center_freq)/bin_width))
+        bin_end = int(round((view_end - center_freq)/bin_width)) + 1
 
     # Prepare data for CSV-ification
     datestr = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d, %H:%M:%S')
-    csv_str = "%s, %d, %d, %.2f, %d, "%(datestr, start_freq, end_freq, bandwidth/len(data), len(data))
+    csv_str = "%s, %d, %d, %.2f, %d, "%(datestr, view_start, view_end, bin_width, len(data))
 
-    csv_str += ", ".join(["%.2f"%(x) for x in db(DATA[:num_bins])]) + "\n"
+    csv_str += ", ".join(["%.2f"%(x) for x in db(DATA[bin_start:bin_end])]) + "\n"
 
     # Send csv_str to file_worker
     q_file.put(csv_str)
@@ -235,6 +282,58 @@ def rx_callback(device, stream, meta_data, samples, num_samples, user_data):
         return stream.next()
 
 
+def freq_planning(start_freq, end_freq, bin_width, fmbw2):
+    """
+    Given frequency parameters, returns a list of (center_frequency,
+    lower_sideband) tuples, denoting the center frequency of each tuning view,
+    and whether we should pay attention to the lower sideband or upper sideband
+    when tuning to a particular frequency.
+
+    Parameters
+    ----------
+    start_freq : float (Hz)
+        Beginning of desired frequency range
+
+    end_freq : float (Hz)
+        End of desired frequency range, must be greater than start_freq
+
+    bin_width : float (Hz)
+        Width of analysis FFT bins
+
+    analysis_bandwidth : float (Hz)
+        Width of analysis window in Hz, equal to filter_margin * bandwidth/2
+
+    Returns
+    -------
+    freqs : list of (center_frequency, lower_sideband) tuples
+        A list of views describing a center frequency to tune to and which
+        sideband to observe, upper or lower (true signifies lower sideband).
+    """
+    import bladeRF
+
+    # First frequency is always the same; either just below start_freq or at
+    # start_freq + bandwidth/2 - binwidth, in the case that start_freq is really
+    # close to the minimum frequency we can tune to:
+    if start_freq - bin_width >= bladeRF.FREQUENCY_MIN:
+        # Put center_freq just below start_freq if we are not at the minimum frequency
+        freqs = [(start_freq - bin_width, False)]
+    else:
+        # Otherwise, put center_freq just above start_freq + bandwidth/2
+        freqs = [(start_freq + fmbw2, True)]
+
+    # Can we get this done with just a single view?
+    if end_freq - start_freq < fmbw2:
+        return freqs
+
+    # Otherwise, let's figure out how many views we need after the first one
+    num_views = int(ceil((end_freq - start_freq)/fmbw2 - 1))
+    freqs += [(start_freq + fmbw2 - bin_width + idx*fmbw2, False) for idx in range(num_views)]
+
+    # Return these frequencies!
+    return freqs
+
+
+
 ################################################################################
 ## MAIN LOOP
 ################################################################################
@@ -244,6 +343,7 @@ def main():
     import threading
     from Queue import Queue
     import scipy.signal
+    import bladeRF
     args = get_args()
     try:
         device = bladeRF.Device(args['--device'])
@@ -259,18 +359,28 @@ def main():
     device.rx.sample_rate = intish(args['--bandwidth'])
 
     freq_arg = args['<lower:upper:bin_width>']
+    filter_margin = float(args['--filter-margin'])
     start_freq, end_freq, bin_width = [floatish(x) for x in freq_arg.split(':')]
     start_freq = max(start_freq, bladeRF.FREQUENCY_MIN)
     end_freq = min(end_freq, bladeRF.FREQUENCY_MAX)
 
-    num_freqs = ceil((end_freq - start_freq)/bandwidth)
+    if end_freq <= start_freq:
+        sys.stderr.write("ERROR: end frequency must be greater than start frequency!\n")
+        return
+
+    # fft_len is the minimum length FFT that guarantees us bins of less than or
+    # equal width as requested through bin_width:
     fft_len = int(ceil(bandwidth/bin_width))
-    freqs = (start_freq + bandwidth/2) + bandwidth * arange(num_freqs)
-    # Clamp freqs because we may attempt to overstep our bounds due to the ceil
-    # above when calculating num_freqs.  This is okay, usually, except when we
-    # exceed FREQUENCY_MAX.
-    if freqs[-1] > bladeRF.FREQUENCY_MAX:
-        freqs[-1] = bladeRF.FREQUENCY_MAX
+
+    # Now that we know our actual fft length, find the true bin width:
+    bin_width = bandwidth/fft_len
+
+    # fmbw2 is the amount of spectrum we get with each view, we quantize to our
+    # effective bin_width given our bandwidth and number of bins
+    fmbw2 = round(filter_margin*(bandwidth/2)*fft_len)/fft_len
+
+    freqs = freq_planning(start_freq, end_freq, bin_width, fmbw2)
+    num_views = len(freqs)
 
     device.lna_gain = int_or_attr(args['--lna-gain'])
     device.rx.vga1 = int_or_attr(args['--rx-vga1'])
@@ -283,11 +393,8 @@ def main():
     # Start FFT worker pool
     num_workers = int(args['--num-workers'])
     window_func = getattr(scipy.signal, args['--window-type'])
-    demean = args['--demean']
     outfile = args['--file']
-    compress = bool(args['-z'])
-    if compress and outfile[-3:] != '.gz':
-        outfile += '.gz'
+    compress = bool(args['--compress'])
     pool, file_process, q_file = start_worker_pool(num_workers, outfile, compress)
 
     # Timing stuffage
@@ -310,22 +417,24 @@ def main():
         }
 
         # Initialize device.rx.frequency, then start the stream doing its thing
-        device.rx.frequency = freqs[0]
+        device.rx.frequency = freqs[0][0]
         stream = device.rx.stream(rx_callback, num_buffers, bladeRF.FORMAT_SC16_Q11, num_samples, num_transfers, user_data=rx_data)
         threading.Thread(target=run_stream, args=(stream,)).start()
 
+        sys.stderr.write("Scanning from %sHz to %sHz, using %d views of %sHz with %d bins %sHz wide\n"%(suffixed(start_freq), suffixed(end_freq), num_views, suffixed(fmbw2), fft_len/2, suffixed(bin_width)))
+
         # Now zoom through frequencies like it's your day off
         freq_idx = 0
-        while curr_time - start_time <= exit_timer:
+        while exit_timer == 0 or curr_time - start_time <= exit_timer:
             samples_received = q_data.get()
 
             # Now that we have the data, apply it to the pool
-            args = (rx_data['data'], window_func, demean, device.rx.frequency, device.rx.bandwidth, end_freq, curr_time, q_file)
+            args = (rx_data['data'], window_func, freqs[freq_idx][1], device.rx.frequency, fmbw2, bin_width, start_freq, end_freq, curr_time, q_file)
             pool.apply_async(analyze_view, args)
 
             # If not, move on to the next frequency
             freq_idx = (freq_idx + 1)%len(freqs)
-            device.rx.frequency = freqs[freq_idx]
+            device.rx.frequency = freqs[freq_idx][0]
             rx_data['data_idx'] = 0
             if freq_idx == 0:
                 curr_time = time()
@@ -334,9 +443,15 @@ def main():
             num_space = int((total_space - 1)*freq_idx/len(freqs))
             tct = time()
             status_line = "[" + " "*num_space + "." + " "*(total_space - num_space - 1) + "]"
-            status_line += " %.1fs/%.1f%%\r"%(tct - start_time, min(100*(tct - start_time)/exit_timer, 100))
-            sys.stdout.write(status_line)
-            sys.stdout.flush()
+
+            if exit_timer > 0:
+                pct_done = "%.1f%%"%(min(100*(tct - start_time)/exit_timer, 100))
+            else:
+                pct_done = u"\u221e"
+
+            status_line += " %.1fs/%s\r"%(tct - start_time, pct_done)
+            sys.stderr.write(status_line)
+            sys.stderr.flush()
     except KeyboardInterrupt:
         pass
     finally:
